62d919444c2e189e801b7b2dba2f3984e6c6aa4f
Rutuja9696 <rutuja.patil@contentstact.com>
2021-02-23 17:38:37 +0530

    added function to delete task and life cycle(didUpdate)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -27,7 +27,38 @@ class TodoList extends Component {
           console.log(err);
         });
     };
+    this.deleteTask = (event) => {
+      let id = event.target.parentNode.querySelector("span").id;
+      fetch(url + id, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
     this.componentDidMount = (event) => {
+      fetch(url)
+        .then(() => {
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({ taskName: [...data.data] });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    this.componentDidUpdate = (event) => {
       fetch(url)
         .then((response) => {
           return response.json();
@@ -45,14 +76,15 @@ class TodoList extends Component {
     return (
       <div>
         <form onSubmit={this.submitForm}>
-          <label>Task list </label>
+          <label>Task list : </label>
           <input type="text" name="task" />
-          <input type="submit" value="Add task" />
+          <input type="submit" value="+ Add task" />
         </form>
         {this.state.taskName.map((task, i) => {
           return (
             <div key={i}>
               <span id={task.taskId}>{task.taskName}</span>
+              <input type="button" value="X" onClick={this.deleteTask} />
             </div>
           );
         })}
