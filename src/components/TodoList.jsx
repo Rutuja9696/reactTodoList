59ce5e44f061ec3348b6f2a97b8166ceb7b228ef
Rutuja9696 <rutuja.patil@contentstact.com>
2021-02-23 12:10:31 +0530

    created function to submit form and created state

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,42 @@
 import { Component } from "react";
+import url from "./ApiCall";
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskName: [],
+    };
+    this.submitForm = (event) => {
+      event.preventDefault();
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ taskName: event.target.task.value }),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          let taskList = [...this.state.taskName];
+          taskList.push(data.data);
+          this.setState({ taskName: taskList });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  }
   render() {
     return (
       <div>
-        <label>Task list </label>
-        <input type="text" />
-        <input type="submit" value="Add task" />
+        <form onSubmit={this.submitForm}>
+          <label>Task list </label>
+          <input type="text" name="task" />
+          <input type="submit" value="Add task" />
+        </form>
       </div>
     );
   }
